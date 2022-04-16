6fc4ff3e94143c1a8f5cfa96d66fbc1cf81dc87c
Marc Steven Clemen <mackyclemen@gmail.com>
2022-04-17 07:59:08 +0800

    Regex fix and enable default settings

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+// initialize setting
+chrome.runtime.onInstalled.addListener((details) => {
+    if(details.reason == 'install') {
+        // set initial settings upon extension install
+        chrome.storage.local.set({enable: true});
+    }
+})
+
 // watch for changes and apply accordingly
 chrome.storage.onChanged.addListener((changes, area) => {
     console.log("storage changed", changes, area);
@@ -6,10 +14,20 @@ chrome.storage.onChanged.addListener((changes, area) => {
             chrome.declarativeNetRequest.updateEnabledRulesets({
                 enableRulesetIds: ['shorts_to_watch_url_converter']
             });
+            chrome.tabs.onUpdated.addListener(eventListener);
         } else {
             chrome.declarativeNetRequest.updateEnabledRulesets({
                 disableRulesetIds: ['shorts_to_watch_url_converter']
             });
+            chrome.tabs.onUpdated.removeListener(eventListener);
         }
     }
 })
+
+// watch for skipped requests and refresh the tab to trigger the ruleset
+let eventListener = (tabID, changeInfo, tab) => {
+    const shortsRegex = "^.*://.*youtube\\.com/shorts/(.+)"
+    if(changeInfo.status == 'complete' && tab.url.match(shortsRegex)) {
+        chrome.tabs.reload(tabID);
+    }
+}
